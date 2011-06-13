75771456fc884217531d55a6a61e7a51bd73e2bf
Peter Molfese <pete@Peter-Molfeses-MacBook-Pro.local>
2011-06-13 19:58:49 -0400

    Initial Commit

---
+++ haskins_fmri_timer/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  haskins_fmri_timer
+//
+//  Created by Peter Molfese on 6/13/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+    // insert code here...
+    NSLog(@"Hello, World!");
+
+    [pool drain];
+    return 0;
+}
+
